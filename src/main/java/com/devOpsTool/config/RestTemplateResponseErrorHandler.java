4e5fa6224f772947be595ac868b0401c642d26f9
Digvijay Singh <Digvijay.Singh@BDINDIA.LOCAL>
2019-02-27 18:55:42 +0530

    updated code

---
+++ src/main/java/com/devOpsTool/config/RestTemplateResponseErrorHandler.java
@@ -0,0 +1,35 @@
+package com.devOpsTool.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
+import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
+
+public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+    @Override
+    public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
+        return (
+                clientHttpResponse.getStatusCode().series() == CLIENT_ERROR
+                        || clientHttpResponse.getStatusCode().series() == SERVER_ERROR);
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
+        if (clientHttpResponse.getStatusCode()
+                 == HttpStatus.TOO_MANY_REQUESTS) {
+
+            
+
+        } else if (clientHttpResponse.getStatusCode()
+                .series() == HttpStatus.Series.CLIENT_ERROR) {
+            // handle CLIENT_ERROR
+            if (clientHttpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new IOException();
+            }
+        }
+    }
+}
