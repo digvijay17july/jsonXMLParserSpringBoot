a4a5ff4c38e5cd3a88150c257a74a01a24d383e2
digvijay17july <digvijay17july@gmail.com>
2019-02-28 02:36:42 +0530

    updated service

--- src/main/java/com/devOpsTool/service/ParserServiceImpl.java
+++ src/main/java/com/devOpsTool/service/ParserServiceImpl.java
@@ -5,6 +5,8 @@ import com.devOpsTool.dao.ParserDao;
 import com.devOpsTool.exception.RestClientException;
 import com.devOpsTool.jamaServices.JamaDomainObject;
 import com.devOpsTool.jamaServices.JamaInstance;
+import com.devOpsTool.json.SimpleJsonSerializer;
+import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,7 @@ import java.util.List;
 
 @Component
 public class ParserServiceImpl implements ParserService {
-    private Logger LOGGER= LoggerFactory.getLogger(ParserServiceImpl.class);
+    private Logger LOGGER = LoggerFactory.getLogger(ParserServiceImpl.class);
     @Autowired
     JamaInstance jamaInstance;
 
@@ -27,27 +29,33 @@ public class ParserServiceImpl implements ParserService {
     @Override
     public String getDataFromServer() {
         LOGGER.info("Entering.. ParserServiceImpl.getDataFromServer");
-        List<JamaDomainObject> projectJsonData= null;
+        List<JamaDomainObject> projectJsonData = null;
+        SimpleJsonSerializer simpleJsonSerializer = new SimpleJsonSerializer();
+        JSONArray jsonArray = new JSONArray();
+
         try {
             projectJsonData = jamaInstance.getAll(jamaConfig.getResource());
+            for (JamaDomainObject jamaDomainObject : projectJsonData) {
+                jsonArray.put(jamaDomainObject);
+            }
         } catch (RestClientException e) {
             e.printStackTrace();
         }
         LOGGER.info("EXit.. ParserServiceImpl.getDataFromServer");
-        return projectJsonData.toString();
+        return jsonArray.toString();
     }
 
     @Override
-    public String getXmlData(String jsonObject) throws Exception{
-        LOGGER.info("Entering.. ParserServiceImpl.getXmlData with Data : ${}",jsonObject);
-        String projectXmlData=parserDao.getXmlData(jsonObject);
+    public String getXmlData(String jsonObject) throws Exception {
+        LOGGER.info("Entering.. ParserServiceImpl.getXmlData with Data : ${}", jsonObject);
+        String projectXmlData = parserDao.getXmlData(jsonObject);
         LOGGER.info("EXit.. ParserServiceImpl.getXmlData");
         return projectXmlData;
     }
 
     @Override
-    public void createXMLFile(String xmlData) throws Exception{
-        LOGGER.info("Entering.. ParserServiceImpl.createXMLFile with Data : ${}",xmlData);
+    public void createXMLFile(String xmlData) throws Exception {
+        LOGGER.info("Entering.. ParserServiceImpl.createXMLFile with Data : ${}", xmlData);
         parserDao.createXMLFile(xmlData);
         LOGGER.info("EXit.. ParserServiceImpl.createXMLFile");
 
